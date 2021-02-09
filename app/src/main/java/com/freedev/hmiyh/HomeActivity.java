9eaaa6bfc04c0cd19e4c05fa6e67b3f6f3616e95
Andron323 <opamil323andron@gmail.com>
2021-02-09 21:39:01 +0200

    1.0

--- app/src/main/java/com/freedev/hmiyh/HomeActivity.java
+++ app/src/main/java/com/freedev/hmiyh/HomeActivity.java
@@ -39,15 +39,7 @@ public class HomeActivity extends AppCompatActivity {
 
 
 
-//        GraphView graph = (GraphView) findViewById(R.id.graph);
-//        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
-//                new DataPoint(0, 1),
-//                new DataPoint(1, 5),
-//                new DataPoint(2, 3),
-//                new DataPoint(3, 2),
-//                new DataPoint(4, 6)
-//        });
-//        graph.addSeries(series);
+
 
         LineChart chartEntity = findViewById(R.id.lineChart);
         ChartEntity firstChartEntity = new ChartEntity(Color.WHITE, graph1);//invisible
